b113078a37757892e7214daba78a7a05ced9087c
Mahmoud Abdul Ghaffar <mghaffar@tmcmf.com>
2026-01-10 05:05:00 +0200

    [feat] create project files for todo service | todo-web-service

---
+++ todo-web-service/src/main/java/org/example/todo_web_service/TodoWebServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.todo_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWebServiceApplication.class, args);
+	}
+
+}
