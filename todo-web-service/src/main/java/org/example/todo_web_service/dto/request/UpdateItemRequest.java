24cc4fade76e8a6e6cd0eb1ca0617db9197e338b
Mahmoud Abdul Ghaffar <mghaffar@tmcmf.com>
2026-01-29 17:39:40 +0200

    feat(todo-service): add backend validations

--- todo-web-service/src/main/java/org/example/todo_web_service/dto/request/UpdateItemRequest.java
+++ todo-web-service/src/main/java/org/example/todo_web_service/dto/request/UpdateItemRequest.java
@@ -6,8 +6,13 @@ import org.example.todo_web_service.entities.Priority;
 import org.example.todo_web_service.entities.TodoStatus;
 
 public record UpdateItemRequest(
-        @Size(max=200) String title,
-        @Size(max=500) String description,
+        @Size(max = 200, message = "title must be at most 200 characters")
+        String title,
+
+        @Size(max = 500, message = "description must be at most 500 characters")
+        String description,
+
         Priority priority,
+
         TodoStatus status
 ) {}
